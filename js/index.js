d83cd39ac9e8cc606c0b0cca46bd473ed8ea76ef
Cesar Val Leiva <cesarvalleiva@gmail.com>
2021-11-29 18:52:14 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,44 @@
+// // ES5
+// var nombre = "Cesar";
+
+// // ES6
+// let nombre = "Cesar";
+
+// nombre = "Jorge"
+
+// const dni = 25980786;
+
+
+// const githubUser = "cesarvalleiva"
+// let salario = 10;
+
+// salario = salario*1.20;
+
+// let nombre = "Cesar"; // string
+// let apellido = "Val Leiva"; // string
+// let telefono = 1534253647; // number
+// let fumador = false; // boolean
+// let caja =  null; // null
+// let salario; // undefined
+
+let s = 10;
+let t = "soy t"
+
+// let saludo = "Hola, este es un mensaje para "+nombre+" "+apellido; // antes
+// let saludo = `Hola, este es un mensaje para ${nombre} ${apellido}`; // ahora
+
+// console.log("hola")
+
+// Ejercicio 1
+// let nombre = "Cesar"
+// console.log(`El nombre del usuario es ${nombre}`)
+
+// alert('Hola David!');
+// prompt('Cuál es tu nombre?');
+
+let nombre = prompt('Cuál es tu nombre?');
+// alert(`Hola ${nombre}`);
+
+let numero1 = 10;
+let numero2 = 20;
+console.log(numero1+numero2);
